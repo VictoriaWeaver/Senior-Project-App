d1070c82c3df339b90c7a57cbafe8f101dfa689e
victoriaweaver <vweaver2013@gmail.com>
2017-06-18 16:07:59 -0400

    Added four empty activities with custom app bar

--- SmartSecuritySystem/app/src/main/java/vi/smartsecuritysystem/MainActivity.java
+++ SmartSecuritySystem/app/src/main/java/vi/smartsecuritysystem/MainActivity.java
@@ -1,13 +1,88 @@
 package vi.smartsecuritysystem;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button familyAccountsBtn;
+    private Button guestAccountsBtn;
+    private Button cameraFeedBtn;
+    private Button userLogsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        familyAccountsBtn = (Button) findViewById(R.id.family_accounts_btn);
+        familyAccountsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FamilyAccountsActivity.class));
+            }
+        });
+        guestAccountsBtn = (Button) findViewById(R.id.guest_accounts_btn);
+        guestAccountsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GuestAccountsActivity.class));
+            }
+        });
+
+        cameraFeedBtn = (Button) findViewById(R.id.camera_feed_btn);
+        cameraFeedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CameraFeedActivity.class));
+            }
+        });
+
+        userLogsBtn = (Button) findViewById(R.id.user_logs_btn);
+        userLogsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, UserLogsActivity.class));
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate our menu from the resources by using the menu inflater.
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
